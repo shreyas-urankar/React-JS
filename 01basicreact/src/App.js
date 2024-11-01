920afe49e1e4ed02b75e7d97295cb4299d426d1f
shreyas-urankar <urankarshreyas@gmail.com>
2024-11-02 00:39:46 +0530

    First commit

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,7 +1,16 @@
+import './App.css';
+import {Header} from './Header';
+import Footer from './Footer';
+
 function App() {
   return (
-    <h1>Hello World!</h1>
-
+    <div className="main">
+      <Header>
+        <div className='container'>
+          <h1>Welcome to Home</h1>
+        </div>
+      </Header>
+    </div>
   );
 }
 
