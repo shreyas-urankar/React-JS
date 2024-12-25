a90322f2f17b4a8023fd4459289ae2adb56ff84a
shreyas-urankar <urankarshreyas@gmail.com>
2024-12-25 23:23:02 +0530

    First commit

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,15 +1,19 @@
-import './App.css';
-import {Header} from './Header';
-import Footer from './Footer';
+import './App.css'; // For App.css file
+import { Header } from './Header'; // For Header component
+import Footer from './Footer'; // For Footer component
+import {Container} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="main">
-      <Header>
-        <div className='container'>
-          <h1>Welcome to Home</h1>
-        </div>
-      </Header>
+      <Header />
+      <Container fluid>
+        hello
+      </Container>
+      <div className="container">
+        <h1 className="text-danger">Welcome to Home</h1>
+      </div>
+      <Footer />
     </div>
   );
 }
