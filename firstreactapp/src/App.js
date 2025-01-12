5955ee8fb7078c4ad67b74ccbf0e2f1b115c8f39
shreyas-urankar <urankarshreyas@gmail.com>
2025-01-12 23:58:44 +0530

    First commit

--- firstreactapp/src/App.js
+++ firstreactapp/src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import doglogo from "./images/dog.jpeg";
+import catlogo from "./images/cat.jpeg";
 
 function App() {
   return (
     <div className="App">
-     <h1 className='text-[40px] text-red-900 font-bold'>Welcome</h1>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '20px' }}>
+        <img 
+          width={200}
+          src="https://images.pexels.com/photos/1851164/pexels-photo-1851164.jpeg?cs=srgb&dl=pexels-charlesdeluvio-1851164.jpg&fm=jpg"
+          alt="A beautiful landscape"
+        />
+        <img 
+          src={doglogo}
+          width={200}
+          alt="Another dog image"
+        />
+        <img src={catlogo}
+        width={250}
+        alt='Cat image'/>
+      </div>
+      <Header />
+      <h1 style={{ fontSize: '40px', color: '#b91c1c', fontWeight: 'bold' }}>Welcome</h1>
     </div>
   );
 }
