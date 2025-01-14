02854a2d2707fb3729be03bfda8e3943a9590cc1
shreyas-urankar <urankarshreyas@gmail.com>
2025-01-14 23:02:55 +0530

    Today I completed with If Else Statement in React JS

--- firstreactapp/src/App.js
+++ firstreactapp/src/App.js
@@ -1,44 +1,68 @@
-import './App.css';
-import Header from './Header';
+import "./App.css";
+import Header from "./Header";
 import doglogo from "./images/dog.jpeg";
 import catlogo from "./images/cat.jpeg";
 import Button from "./Button";
-import { useState } from 'react';
+import { useState } from "react";
 function App() {
-  let[count,setCount]=useState(1)
-  let displayData=()=>{
+
+  let template = "";
+  let [count, setCount] = useState(1);
+  let [pshow, setPshow] = useState(false);
+  if (pshow) {
+    template=<>
+    <button className="pshow.hide" onClick={()=>setPshow(!pshow)}>Hide</button>
+    <p>Welcome to Shreyas's ReactJS</p>
+    </>
+  } 
+  else {
+    template = <button className="pshow" onClick={()=>setPshow(!pshow)}>Show pshow</button>;
+  }
+
+
+  let displayData = () => {
     // alert("We lcome to Shreyas's React Project")
-    setCount(count+1)
+    setCount(count + 1);
   };
 
-  let addData=(a,b)=>{
-    alert(a+b)
+  let addData = (a, b) => {
+    alert(a + b);
   };
+
+
   return (
     <div className="App">
-      {count}
-      <button className='save-button' onClick={displayData} >Save</button>
-      <button className='add-data' onClick={()=>addData(20,25)}>Add Data</button>
-      <img/>
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '20px' }}>
-        <img 
+      {template}
+      <div>{count}</div>
+
+      <button className="save-button" onClick={displayData}>
+        Save
+      </button>
+      <button className="add-data" onClick={() => addData(20, 25)}>
+        Add Data
+      </button>
+      <img />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "20px",
+        }}
+      >
+        <img
           width={200}
           src="https://images.pexels.com/photos/1851164/pexels-photo-1851164.jpeg?cs=srgb&dl=pexels-charlesdeluvio-1851164.jpg&fm=jpg"
           alt="A beautiful landscape"
         />
-        <img 
-          src={doglogo}
-          width={200}
-          alt="Another dog image"
-        />
-        <img src={catlogo}
-        width={250}
-        alt='Cat image'/>
+        <img src={doglogo} width={200} alt="Another dog image" />
+        <img src={catlogo} width={250} alt="Cat image" />
       </div>
       <Header />
-      <h1 style={{ fontSize: '40px', color: '#b91c1c', fontWeight: 'bold' }}>Welcome</h1>
+      <h1 style={{ fontSize: "40px", color: "#b91c1c", fontWeight: "bold" }}>
+        Welcome
+      </h1>
     </div>
-    
   );
 }
 
