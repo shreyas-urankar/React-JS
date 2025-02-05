aebaa173b8c7755c8a2f9a20cfa872fb47d89c93
shreyas-urankar <urankarshreyas@gmail.com>
2025-02-05 23:28:25 +0530

    Building a Todo List App and  Create tabbing

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,15 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { tabs } from "./tabs";
 
 function App() {
+  let [todolist, setTodolist] = useState([]);
+  let [activeTabs, setactiveTabs] = useState(0);
+  let[activeContent, setactiveContant]=useState(tabs[0]);
+  let changeData=(index)=>{
+    setactiveTabs(index)
+    setactiveContant(tabs[index])
+  }
+  let saveToDoList = (event) => {
+    let todoname = event.target.todoname.value;
+    if (!todolist.includes(todoname)) {
+      let finalDolist = [...todolist, todoname];
+      setTodolist(finalDolist);
+    } else {
+      alert("ToDo Name Allready Exists..... ");
+    }
+
+    event.preventDefault();
+  };
+
+  let list = todolist.map((value, index) => {
+    return (
+      <ToDoListItems
+        value={value}
+        key={index}
+        indexNumber={index}
+        todolist={todolist}
+        setTodolist={setTodolist}
+      />
+    );
+  });
   return (
     <div className="App">
+      <div className="tabsOuter">
+        <h1 style={{ textAlign: "left" }}>
+          Law Prep Vision Mission and Values
+        </h1>
+
+        <ul>
+          {tabs.map((tabsItems, index) => {
+            return (
+              <li>
+                <button onClick={()=>changeData(index)} className={activeTabs==index? 'activeButton': ''}>{tabsItems.title}</button>
+              </li>
+            );
+          })}
+
+        </ul>
+        {activeContent!==undefined?<p>
+            {activeContent.description}
+          </p>
+          :
+          ''
+           }
+        
+      </div>
       <h1>Todo List</h1>
-      <form>
-        <input type='text'><button>Save</button></input>
+      <form onSubmit={saveToDoList}>
+        <input type="text" name="todoname" /> <button>Save</button>
       </form>
+      <div className="outerDiv">
+        <ul>{list}</ul>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+function ToDoListItems({ value, indexNumber, todolist, setTodolist }) {
+  let [status, setStatus] = useState(false);
+  let deleteRow = () => {
+    let finalData = todolist.filter((v, i) => i != indexNumber);
+    setTodolist(finalData);
+  };
+
+  let checkStatus = () => {
+    setStatus(!status);
+  };
+  return (
+    <li className={status ? "completetodo" : ""} onClick={checkStatus}>
+      {indexNumber + 1} {value} <span onClick={deleteRow}>&times;</span>
+    </li>
+  );
+}
---
+++ todo-list/src/tabs.jsx
@@ -0,0 +1,14 @@
+export let tabs=[
+    {
+        title: 'Vision',
+        description: 'Law Prep Tutorial envisions becoming the most trusted and result-oriented coaching institute for law aspirants across the country. We aim to revolutionize legal education by providing innovative teaching methodologies, up-to-date materials, and a nurturing environment where students can thrive. Our vision extends beyond just clearing entrance exams; we aspire to mold the next generation of legal professionals who will make a significant impact in the judiciary and society at large.'
+      },
+      {
+        title: 'Mission',
+        description: 'Our mission at Law Prep Tutorial is to provide students with the highest quality education and training necessary to excel in competitive law entrance exams like CLAT, AILET, and others. We are committed to fostering a learning atmosphere that encourages critical thinking, problem-solving, and effective communication skills. By offering personalized mentoring, comprehensive study materials, and mock tests, we aim to guide students through every step of their journey toward a successful legal career.'
+      },
+      {
+        title: 'Values',
+        description: 'At Law Prep Tutorial, we uphold the core values of integrity, excellence, and dedication in all aspects of our work. We believe in maintaining transparency in our teaching practices, ensuring that every student receives fair and unbiased guidance. Our team of experienced educators is passionate about delivering top-notch content and fostering a supportive community where students feel empowered to reach their full potential. We prioritize student success above all, and our values drive us to continuously improve and innovate in the field of legal education.'
+      }
+];
