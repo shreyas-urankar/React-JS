9ff4d606c0eb6419cfc106850314c3a9db96606b
shreyas-urankar <urankarshreyas@gmail.com>
2025-02-08 02:24:33 +0530

    Today i have done with Show userData From State in Table Tag, Email or Phone Number Already Exists, Delete Row in Userdata State

--- user-enquire-handle-with-state-using-controlled-component/src/App.js
+++ user-enquire-handle-with-state-using-controlled-component/src/App.js
@@ -1,40 +1,66 @@
 import "./App.css";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row, Table } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
-
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer,toast } from "react-toastify";
 function App() {
   let [formData, setFormData] = useState({
     uname: "",
     uemail: "",
     uphone: "",
     umessage: "",
-    index:""
+    index: "",
   });
 
-  let[userData, setUserData]=useState([])
-  let getValue=(event)=>{
-    let oldData={...formData}
-    let inputName=event.target.name;
-    let inputValue=event.target.value;
-    oldData[inputName]=inputValue;
-    setFormData(oldData )
-  }
+  let [userData, setUserData] = useState([]);
+  let getValue = (event) => {
+    let oldData = { ...formData };
+    let inputName = event.target.name;
+    let inputValue = event.target.value;
+    oldData[inputName] = inputValue;
+    setFormData(oldData);
+  };
 
-  let handleSubmit=(event)=>{
-    let currentUserFormdata={
-      uname:formData.uname,
-      uemail:formData.uemail,
-      uphone:formData.uphone,
-      umessage:formData.umessage
+  let handleSubmit = (event) => {
+    let currentUserFormdata = {
+      uname: formData.uname,
+      uemail: formData.uemail,
+      uphone: formData.uphone,
+      umessage: formData.umessage,
+    };
+
+    let checkFilterUser = userData.filter(
+      (v) => (v.uemail === formData.uemail || v.uphone === formData.uphone)
+    );
+
+    if (checkFilterUser.length === 1) {
+      toast.error("Email-Id or Phone Number already Exists ")
+    } else {
+      let oldUserData = [...userData, currentUserFormdata];
+      console.log(oldUserData);
+      setUserData(oldUserData);
+      setFormData({
+        uname: "",
+        uemail: "",
+        uphone: "",
+        umessage: "",
+        index: "",
+      });
     }
-    let oldUserData=[...userData, currentUserFormdata]
+    event.preventDefault();
+  };
 
-    setUserData(oldUserData)
-    event.prevebtDefault();
-  }
+  console.log(formData);
+
+  let deleteRow = (indexNumber) => {
+    let filterDataafterDelete = userData.filter((v, i) => i !== indexNumber)
+    toast.success("Data Delete. ")
+    setUserData(filterDataafterDelete)
+  };
   return (
     <Container fluid>
+      <ToastContainer/>
       <Container>
         <Row>
           <Col className="text-center py-5">
@@ -43,36 +69,97 @@ function App() {
         </Row>
         <Row>
           <Col lg={5}>
-          {userData.length}
+            {userData.length}
             <form onSubmit={handleSubmit}>
               <div className="pb-3">
                 <label className="form-label">Name</label>
-                <input type="text" onChange={getValue} value={formData.uname} name="uname" className="form-control" />
+                <input
+                  type="text"
+                  onChange={getValue}
+                  value={formData.uname}
+                  name="uname"
+                  className="form-control"
+                />
               </div>
 
               <div className="pb-3">
                 <label className="form-label">Email</label>
-                <input type="text" onChange={getValue} value={formData.uemail} name="uemail" className="form-control" />
+                <input
+                  type="text"
+                  onChange={getValue}
+                  value={formData.uemail}
+                  name="uemail"
+                  className="form-control"
+                />
               </div>
 
               <div className="pb-3">
                 <label className="form-label">Phone</label>
-                <input type="text" onChange={getValue} value={formData.uphone} name="uphone" className="form-control" />
+                <input
+                  type="text"
+                  onChange={getValue}
+                  value={formData.uphone}
+                  name="uphone"
+                  className="form-control"
+                />
               </div>
 
               <div className="pb-3">
                 <label className="form-label">Message</label>
-                <textarea name="umessage" onChange={getValue} value={formData.umessage} className="form-control" rows="3" />
+                <textarea
+                  name="umessage"
+                  onChange={getValue}
+                  value={formData.umessage}
+                  className="form-control"
+                  rows="3"
+                />
               </div>
 
               <button className="btn btn-primary">
-                {
-                  formData.index!==""?'Update':'Save'
-                }
+                {formData.index !== "" ? "Update" : "Save"}
               </button>
             </form>
           </Col>
-          <Col lg={7}></Col>
+          <Col lg={7}>
+            <br />
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>Name</th>
+                  <th>Email-Id</th>
+                  <th>Phone Number</th>
+                  <th>Message</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {userData.length >= 1 ? (
+                  userData.map((obj, i) => {
+                    return (
+                      <tr key={i}>
+                        <td>{i + 1}</td>
+                        <td>{obj.uname}</td>
+                        <td>{obj.uemail}</td>
+                        <td>{obj.uphone}</td>
+                        <td>{obj.umessage}</td>
+                        <td>
+                          <button onClick={() => deleteRow(i)}>
+                            Delete
+                          </button>
+                          <button>Edit</button>
+                        </td>
+                      </tr>
+                    );
+                  })
+                ) : (
+                  <tr>
+                    <td colSpan={6}>No Data Found</td>
+                  </tr>
+                )}
+              </tbody>
+            </Table>
+          </Col>
         </Row>
       </Container>
     </Container>
