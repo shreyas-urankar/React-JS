08e02c74a544d8b8d1b62f176574edf3c29db210
shreyas-urankar <urankarshreyas@gmail.com>
2025-03-03 23:14:42 +0530

    Added new project

---
+++ react-project-one/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>Hello Jee, My name is Shreyas Urankar</p>
+    </>
+  )
+}
+
+export default App
