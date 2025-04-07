d4ab34ff2005bc6bc3ab09c090a459c93f7ada32
SHREYAS PRASHANT URANKAR <urankarshreyas@gmail.com>
2025-04-07 13:33:50 +0530

    Updated useCallBack hook

--- Using Vite/16-useCallBack-hook/src/App.jsx
+++ Using Vite/16-useCallBack-hook/src/App.jsx
@@ -1,28 +1,29 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  function handleClick(){
-    setCount(count+1)
-  }
+  const handleClick = useCallback(() => {
+    setCount(count + 1);
+  }, [count]);
+  
   return (
     <>
-    <div>
-      Count: {count}
-     </div>
-     <br/>
-     <br/> 
-    <div>
-      <button onClick={handleClick}>
-        Increment
-      </button>
-    </div> 
-    <br/>
-    <br/>
-    <div>
-      <ChildComponent buttonName="Click me"/>
-    </div>
+      <div>
+        Count: {count}
+      </div>
+      <br />
+      <br />
+      <div>
+        <button onClick={handleClick}>
+          Increment
+        </button>
+      </div>
+      <br />
+      <br />
+      <div>
+        <ChildComponent buttonName="Click me" />
+      </div>
     </>
   )
 }
